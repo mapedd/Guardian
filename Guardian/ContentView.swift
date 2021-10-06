4fa357661064ef413fa541e6e2f60594da918f93
Tomek Kuzma <tomasz.kuzma@depop.com>
2021-10-06 17:47:55 +0200

    Initial Commit

---
+++ Guardian/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Guardian
+//
+//  Created by Tomasz Kuzma on 06/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Guardian/GuardianApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuardianApp.swift
+//  Guardian
+//
+//  Created by Tomasz Kuzma on 06/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct GuardianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
